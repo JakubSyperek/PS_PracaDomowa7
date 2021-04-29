694893ae41e26fce61b96b64e48495cac205e5e6
JakubSyperek <76512533+JakubSyperek@users.noreply.github.com>
2021-04-29 17:59:31 +0200

    Add files via upload

---
+++ 5.2.3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#define A "KANAPKA"
+#define B "KANAPKA"
+int main(void)
+{
+    porownaj();
+    printf("%s\n", A);
+    printf("%s\n", B);
+    porownaj();
+}
+
+void porownaj(void)
+{
+   char a[10], b[10];
+   if (strcmp(a,b) == 0)
+      return 1;
+   else
+      return 0;
+}
